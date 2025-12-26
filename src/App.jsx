695b00a8706ea547f57a51b3d87639de8a0246af
Lucas Linhares <lucaslinhares_@outlook.com>
2025-12-26 14:01:20 -0300

    atualiza o cor do texto do botão consulta NCM atualizado

--- src/App.jsx
+++ src/App.jsx
@@ -198,7 +198,7 @@ export default function App() {
                 {/* 🔹 BOTÃO DE CONSULTA DO NCM ATUALIZADO */}
                 <div style={{ display: "flex", justifyContent: "flex-end" }}>
                   <button
-                    style={{ marginTop: "12px", backgroundColor: "#3c763d" }}
+                    style={{ marginTop: "12px", backgroundColor: "#3c763d", color: "#fff" }}
                     onClick={() => consultarNcmAtualizado(resultado.ncm)}
                     >
                     Consultar NCM atualizado
